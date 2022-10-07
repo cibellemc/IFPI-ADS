f97890bcf9cd857d9d780365d109799012be4f71
Cibelle Maciel <cibellemaciel02@gmail.com>
2022-10-06 22:41:52 -0300

    add pasta listas e primeira versão inserir

---
+++ Mod-II/EstruturaDeDados/listas/lista1.cpp
@@ -0,0 +1,47 @@
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <string>
+using namespace std;
+
+typedef struct aluno{
+    int mat;
+    string nome;
+} Aluno;
+
+void inserir(){
+//void inserir(qtdAlunos){
+    int qa = 0;
+    int cont;
+
+    /*int qtdAlunos;
+    printf("Quantidade de alunos a inserir: ")
+    scanf("%d", qtdAlunos)*/
+
+    Aluno turma[5];
+    //Aluno turma[qtdAlunos];
+    //int tamanhoArray = sizeof(turma)/sizeof(Aluno); //o tamanho total de turma dividido por o valor de um aluno
+
+    printf("Inserir novo aluno:\n");
+    do {
+        if (qa < 5){
+            cout<<"\nMatricula - Aluno %d", qa;
+            cin>>turma[qa].mat;
+            cout<<"Nome:";
+            cin>>turma[qa].nome;
+            qa++;
+            cout<<"\nAluno inserido com sucesso.";
+        } else {
+            cout<<"\nNao foi possivel inserir. Turma cheia.";
+            break;
+        }
+
+        cout<<"\nInserir outro? (1-Sim/2-Nao)";
+        cin>>cont;
+    } while(cont == 1);
+}
+
+int main(){
+    inserir();
+    return 0;
+}
