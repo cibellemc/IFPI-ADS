4fb2ba097250f008e6662bbb52b2f8250de3def5
Cibelle Maciel <cibellemaciel02@gmail.com>
2022-10-07 21:26:06 -0300

    atualizando ambientação e lista1

--- Mod-II/EstruturaDeDados/listas/lista1.cpp
+++ Mod-II/EstruturaDeDados/listas/lista1.cpp
@@ -9,27 +9,42 @@ typedef struct aluno{
     string nome;
 } Aluno;
 
+/*Aluno * turma(){
+    int qtdAlunos;
+    cout<<("Quantidade de alunos a inserir: ")
+    cin>>("%d", qtdAlunos)
+    Aluno turma[qtdAlunos];
+    return turma;
+}*/
+
+/*Aluno fila[](int tamanho){
+	Aluno f[tamanho];
+	return f; 
+}*/
+
 void inserir(){
 //void inserir(qtdAlunos){
-    int qa = 0;
+    int i = 0;
     int cont;
 
-    /*int qtdAlunos;
-    printf("Quantidade de alunos a inserir: ")
-    scanf("%d", qtdAlunos)*/
+    int qtdAlunos;
+    cout<<("Quantidade de alunos a inserir: ");
+    cin>>("%d", qtdAlunos);
+    Aluno turma[qtdAlunos];
 
-    Aluno turma[5];
+    int tam = sizeof(turma)/sizeof(Aluno);
+    //Aluno t[tamanho];
     //Aluno turma[qtdAlunos];
     //int tamanhoArray = sizeof(turma)/sizeof(Aluno); //o tamanho total de turma dividido por o valor de um aluno
 
-    printf("Inserir novo aluno:\n");
+    cout<<("Inserir novo aluno:\n");
     do {
-        if (qa < 5){
-            cout<<"\nMatricula - Aluno "<<qa << ": ";
-            cin>>turma[qa].mat;
+        if (i < tam){
+            cout<<"\nMatricula - Aluno "<<i << ": ";
+            cin>>turma[i].mat;
             cout<<"Nome: ";
-            cin>>turma[qa].nome;
-            qa++;
+            cin>>turma[i].nome;
+            i++;
             cout<<"\nAluno inserido com sucesso.";
         } else {
             cout<<"\nNao foi possivel inserir. Turma cheia.";
@@ -41,7 +56,54 @@ void inserir(){
     } while(cont == 1);
 }
 
+/*void remover(Aluno  turma[]){
+    int matprocurada, i, cont, achou, resp;
+    do{
+        cout<<"\nRemover aluno: ";
+        cont<<"\nMatrícula do aluno: ";
+        cin>>matprocurada;
+        achou = procura(matprocurada);
+        if(achou !=-1){
+            //mostre(achou);
+            cout<<procura()
+            cout<<"\nDeseja remover o aluno? (1-Sim/2-Nao)";
+            cin>>resp;
+            if(resp ==1){
+                cout<<"\nAluno removido com sucesso!";
+            } else{
+                cout<<"\nO aluno não foi removido!";
+                break;
+            }
+        } else{
+            cout<<"\nNúmero de matrícula não encontrado.";
+        }
+
+        cout<<"\nRemover outro? (1-Sim/2-Nao)";
+        cin>>cont;
+    } while (cont == 1);
+}
+
+int procura(Aluno  turma[], int matprocurada){
+    int i, matprocurada;
+    cout<<"\nConsultar aluno";
+    cout<<"\nMatrícula do aluno: ";
+    cin>>matprocurada;
+
+    int tamanhoArray = sizeof(turma)/sizeof(Aluno);
+    for (i = 0; i < tamanhoArray; i++){
+        if (turma[i].mat === matprocurada ){
+            return turma[i].mat;
+            break;
+        } 
+    }
+}
+
+void mostre(Aluno * a){
+    cout<<("%d", a );
+}*/
+
 int main(){
     inserir();
+    //cout<<("\n%d", procura(inserir()));
     return 0;
 }
