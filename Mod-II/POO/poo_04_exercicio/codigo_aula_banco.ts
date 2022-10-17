a9e7c2561a226d077db6a96c87890c744aad4947
Cibelle Maciel <cibellemaciel02@gmail.com>
2022-10-17 20:50:06 -0300

    exportando classes Person e Conta

--- Mod-II/POO/poo_04_exercicio/codigo_aula_banco.ts
+++ Mod-II/POO/poo_04_exercicio/codigo_aula_banco.ts
@@ -1,4 +1,4 @@
-class Person {
+export class Person {
     nome: string;
     constructor(nome: string) {
         this.nome = nome;
@@ -9,7 +9,7 @@ let p : Person = new Person("ely");
 console.log(p.nome);
 */
 
-class Conta {
+export class Conta {
     numero: string;
     saldo: number;
     cliente: Person;
@@ -47,7 +47,7 @@ class Conta {
     }
 }
 
-class Banco {
+/*class Banco {
     contas: ContaBancaria[] = [];
 
     inserir(conta: ContaBancaria): void {
@@ -70,20 +70,22 @@ class Banco {
     transfeir(numeroOrigem: string, numeroDestino: string, valor: number): void {
 
     }
+}*/
+
+function main(){
+    let c1: Conta = new Conta("1",100, new Person("C")); // conta nº 1, de titular C inicializa com  r$100
+    let c2: Conta = new Conta("2",100, new Person("M")); // conta nº 2, de titular M inicializa com  r$100
+    let c3: Conta; // defino o  tipo de c3
+    
+    c1 = c2; // c1 aponta pra c2, no caso passa a assumir os valores que c2 assumir
+    c3 = c1; // c3 aponta pra c1, no caso passa a assumir os valores que c1 assumir
+    // c3 -> c1 -> c2 : só muda c2
+    
+    c1.sacar(10); // é retirado 10 dos 100
+    c1.transferir(c2,50); // tira 50 de c1 e poe em c2, porem c1"é c2"
+    
+    console.log(c1.consultarSaldo());
+    console.log(c2.consultarSaldo()); 
+    console.log(c3.consultarSaldo()); 
+
 }
-
-
-let c1: Conta = new Conta("1",100, new Person("C")); // conta nº 1, de titular C inicializa com  r$100
-let c2: Conta = new Conta("2",100, new Person("M")); // conta nº 2, de titular M inicializa com  r$100
-let c3: Conta; // defino o  tipo de c3
-
-c1 = c2; // c1 aponta pra c2, no caso passa a assumir os valores que c2 assumir
-c3 = c1; // c3 aponta pra c1, no caso passa a assumir os valores que c1 assumir
-// c3 -> c1 -> c2 : só muda c2
-
-c1.sacar(10); // é retirado 10 dos 100
-c1.transferir(c2,50); // tira 50 de c1 e poe em c2, porem c1"é c2"
-
-console.log(c1.consultarSaldo());
-console.log(c2.consultarSaldo()); 
-console.log(c3.consultarSaldo()); 
