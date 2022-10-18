8f565f33e26968b3f7f72a1cb55c2a5297e4b898
Cibelle Maciel <cibellemaciel02@gmail.com>
2022-10-17 22:25:38 -0300

    q2 - incompleta (erro ao exibir mais curtida)

--- Mod-II/POO/poo_05_exercicio/q2-microblog.ts
+++ Mod-II/POO/poo_05_exercicio/q2-microblog.ts
@@ -9,8 +9,8 @@ class Postagem{
         this.quantidadeCurtidas = q
     }
 
-    curtir(): void{
-        this.quantidadeCurtidas++
+    curtir(qtdLikes: number): void{
+        this.quantidadeCurtidas += qtdLikes
     }
 
     toString(): void{
@@ -73,43 +73,28 @@ class Microblog{
 function m(){
 
     let postagem1 : Postagem = new Postagem(1, "Testando código!! :)", 0)
-   
     let postagem2 : Postagem = new Postagem(2, "Acabei de conhecer o método Math.max!! DAORAA", 0)
     let postagem3 : Postagem = new Postagem(3, "Tenho até 22h pra terminar!! SOS", 0)
-
     let postagem4 : Postagem = new Postagem(4, "Preciso implementar um if pra não permitir postagem com id igual", 0)
     let postagem5 : Postagem = new Postagem(5, "#typescript", 0)
+
     let blog : Microblog = new Microblog()
+
     blog.novaPostagem(postagem1)
     blog.novaPostagem(postagem2)
     blog.novaPostagem(postagem3)
     blog.novaPostagem(postagem4)
     blog.novaPostagem(postagem5)
-    postagem1.curtir()
-    postagem1.curtir()
-    postagem1.curtir()
-    postagem1.curtir()
-    postagem1.curtir()
-    postagem2.curtir()
-    postagem2.curtir()
-    postagem2.curtir()
-    postagem3.curtir()
-    postagem3.curtir()
-    postagem3.curtir()
-    postagem3.curtir()
-    postagem3.curtir()
-    postagem3.curtir()
-    postagem3.curtir()
-    postagem3.curtir()
-    postagem4.curtir()
 
-    //postagem.curtir()
-    /*postagem.curtir()
-    postagem.curtir()
-    postagem.curtir()
-    postagem.curtir()
-    postagem.curtir()*/
-    //postagem.toString()
+    postagem1.curtir(5)
+    postagem2.curtir(3)
+    postagem3.curtir(0)
+    postagem4.curtir(1)
+
+    /*console.log(postagem1.quantidadeCurtidas)
+    console.log(postagem2.quantidadeCurtidas)
+    console.log(postagem3.quantidadeCurtidas)
+    console.log(postagem4.quantidadeCurtidas)*/
 
     blog.maisCurtida()
 }
