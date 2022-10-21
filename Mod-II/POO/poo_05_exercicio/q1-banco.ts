d3b1f7b37fda7598a1c2f7d9653bfdfa583ead5d
Cibelle Maciel <cibellemaciel02@gmail.com>
2022-10-20 22:37:54 -0300

    separando e exportando classe banco - criação do arquivo teste

--- Mod-II/POO/poo_05_exercicio/q1-banco.ts
+++ Mod-II/POO/poo_05_exercicio/q1-banco.ts
@@ -1,6 +1,6 @@
-import { Conta, Person } from "../poo_04_exercicio/codigo_aula_banco";
+import { Conta } from "../poo_04_exercicio/codigo_aula_banco";
 
-class Banco {
+export class Banco {
     contas: Conta[] = [];
 
     inserir(c: Conta): void {
@@ -87,27 +87,3 @@ class Banco {
     }
 }
 
-function main(){
-let c4: Conta = new Conta("4", 100, new Person("D"))
-let c5: Conta = new Conta("5", 300, new Person("E"))
-let c6: Conta = new Conta("6", 300, new Person("F"))
-let c7: Conta = new Conta("7", 0, new Person("F"))
-
-let b: Banco = new Banco()
-
-b.inserir(c4)
-b.inserir(c5)
-b.inserir(c5)
-b.inserir(c6)
-b.inserir(c7)
-
-b.transfeir("5", "60", 150)
-
-console.log("Saldo da conta c4: R$" +c4.consultarSaldo().toFixed(2))
-console.log("Saldo da conta c5: R$" + c5.consultarSaldo().toFixed(2))
-console.log(`O valor total armazanado no banco é R$${b.dinheiroNoBanco().toFixed(2)}`)
-console.log(`A média de saldos no banco é R$${b.mediaDeSaldos().toFixed(2)}`)
-
-}
-
-main()
