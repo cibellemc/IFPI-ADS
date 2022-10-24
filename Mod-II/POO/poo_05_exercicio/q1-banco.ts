1f48f52d073f9a70a583cead23f87cb48ca6cf91
Cibelle Maciel <cibellemaciel02@gmail.com>
2022-10-23 21:04:13 -0300

    privando array de contas e método consultar por índice

--- Mod-II/POO/poo_05_exercicio/q1-banco.ts
+++ Mod-II/POO/poo_05_exercicio/q1-banco.ts
@@ -1,57 +1,65 @@
 import { Conta } from "../poo_04_exercicio/codigo_aula_banco";
 
+// alguns métodos alterados para boolean, para facilitar a implementação no app.
+// em caso falso retorno mensagem de erro. 
+
 export class Banco {
-    contas: Conta[] = [];
+    private contas: Conta[] = [];
 
     inserir(c: Conta): void {
-        if (this.consultarIndice(c.numero) == -1){ //se não houver índice no array
+        if (this.consultarIndice(c.numeroConta) == -1){ //se não houver índice no array
             this.contas.push(c);
         }
     }
 
     alterar(c: Conta): void {
-        let indice = this.consultarIndice(c.numero);
+        let indice = this.consultarIndice(c.numeroConta);
 
         if (indice != -1) {
             this.contas[indice] = c;
         }
     }
 
-    excluir(numero: string): void{
+    excluir(numero: string): boolean{
         let indice: number = this.consultarIndice(numero);
         if (indice != -1) {
             for (let i: number = indice; i < this.contas.length; i++) {
                 this.contas[i] = this.contas[i + 1];
             }
         this.contas.pop();
-        }
+        return true
+        } return false
     }
 
-    sacar(numero: string, valor: number): void {
+    sacar(numero: string, valor: number): boolean {
         let conta: Conta = this.consultar(numero);
-        if (conta != null) {
+        if (conta != null && conta.Saldo - valor >= 0) {
             conta.sacar(valor);
-        }
+            return true
+        } return false
     }
 
-    depositar(numero: string, valor: number): void {
+    depositar(numero: string, valor: number): boolean {
         let conta: Conta = this.consultar(numero);
         if (conta != null) {
             conta.depositar(valor);
-        }
+            return true
+        } return false
     }
 
-    transfeir(numeroCredito: string, numeroDebito: string, valor: number): void {
+    transfeir(numeroCredito: string, numeroDebito: string, valor: number): boolean {
         if (this.consultar(numeroCredito)!= null && this.consultar(numeroDebito)!= null ){ //se ambas forem válidas
-            this.sacar(numeroCredito, valor)
-            this.depositar(numeroDebito, valor)
-        }
-    }
+            if(this.sacar(numeroCredito, valor)) {
+                this.depositar(numeroDebito, valor) 
+                return true
+            }
+        }  return false
+}
 
     consultar(numero: String): Conta {
         let contaProcurada!: Conta;
         for (let c of this.contas) {
-            if (c.numero == numero) {
+            if (c.numeroConta == numero) {
             contaProcurada = c;
             break;
             }
@@ -59,10 +67,10 @@ export class Banco {
         return contaProcurada;
     }
 
-    consultarIndice(numero: String): number {
+    private consultarIndice(numero: string): number {
         let indice: number = -1;
         for (let i: number = 0; i < this.quantidadeDeContas(); i++) {
-            if (this.contas[i].numero == numero) {
+            if (this.contas[i].numeroConta == numero) {
                 indice = i;
                 break;
             }
@@ -77,7 +85,7 @@ export class Banco {
     dinheiroNoBanco(): number{
         let dinheiroTotal: number = 0
         for (let c of this.contas) {
-            dinheiroTotal += c.consultarSaldo()
+            dinheiroTotal += c.Saldo
         }
         return dinheiroTotal;
     }
@@ -85,5 +93,10 @@ export class Banco {
     mediaDeSaldos(): number{
         return this.dinheiroNoBanco() / this.quantidadeDeContas()
     }
+
+    get Contas(){
+        return this.contas //consertar: ta mostrando as duas
+    }
+
 }
 
