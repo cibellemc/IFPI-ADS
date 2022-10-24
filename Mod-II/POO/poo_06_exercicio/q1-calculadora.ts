840ab171ed0a1a6ea929a0a6200b0a21aec13287
Cibelle Maciel <cibellemaciel02@gmail.com>
2022-10-23 23:59:01 -0300

    criando métodos get para os dois operadores

--- Mod-II/POO/poo_06_exercicio/q1-calculadora.ts
+++ Mod-II/POO/poo_06_exercicio/q1-calculadora.ts
@@ -1,4 +1,4 @@
-class Calculadora{
+export class Calculadora{
     private operando1: number
     private operando2: number
 
@@ -10,25 +10,33 @@ class Calculadora{
     soma(): number{
         return this.operando1 + this.operando2
     }
+    
     subtracao():number{
         return this.operando1 - this.operando2
     }
+
     multiplicacao(): number{
         return this.operando1 * this.operando2
     }
+
     divisao(): number{
         return this.operando1 / this.operando2
     }
 
-    get operandos(){
-        return (`Operando 1: ${this.operando1}\nOperando 2: ${this.operando2}`)
+    get operandoUm(){
+        return this.operando1
+    }
+    
+    get operandoDois(){
+        return this.operando2
     }
 }
 
-let c : Calculadora = new Calculadora(1,2)
-console.log(c.soma())
-console.log(c.subtracao())
-console.log(c.multiplicacao())
-console.log(c.divisao())
-console.log(c.operandos); //com get nao precisa botar ()
-
+function main (){
+    let c : Calculadora = new Calculadora(1,2)
+    console.log(c.soma())
+    console.log(c.subtracao())
+    console.log(c.multiplicacao())
+    console.log(c.divisao())
+    console.log(c.operandoUm); //com get nao precisa botar ()
+}
