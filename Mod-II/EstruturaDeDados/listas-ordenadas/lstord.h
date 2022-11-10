4d0b8eb44479baa56f74fc9a26674fa40ea38156
Cibelle Maciel <cibellemaciel02@gmail.com>
2022-11-10 17:43:55 -0300

    capítulo 10 - listas ordenadas

--- Mod-II/EstruturaDeDados/listas-ordenadas/lstord.h
+++ Mod-II/EstruturaDeDados/listas-ordenadas/lstord.h
@@ -1,15 +1,15 @@
 # include "../listas/listaencadeada.h"
 
-void ins(Item x, Lista *L){
+void ins(Item x, Lista *L){ // insere ordenado
 	while(*L != NULL && (*L)->item < x){
 		L = &(*L)->prox; 
 	}
 	*L = no(x, *L); 
 }
 
-void rem(Item x, Lista *L){
+void rem(Item x, Lista *L){ // remove o valor
 	while(*L != NULL && (*L)->item < x){
-		if(*L == NULL || (*L)->item > x){
+		if(*L == NULL || (*L)->item > x){ // se for maior que o valor, não pertence a lista
 			return;
 		}
 	}
@@ -18,7 +18,7 @@ void rem(Item x, Lista *L){
 	free(n);
 }
 
-int em(Item x, Lista L){
+int em(Item x, Lista L){ 
 	while(L != NULL && (L)->item < x){
 		L = L->prox;
 	}
