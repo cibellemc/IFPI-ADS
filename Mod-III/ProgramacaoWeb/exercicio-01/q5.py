abf1c0162784b59df8d532efe05b2c180ed584ec
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-02-21 23:50:46 -0300

    q5 - script de busca

--- Mod-III/ProgramacaoWeb/exercicio-01/q5.py
+++ Mod-III/ProgramacaoWeb/exercicio-01/q5.py
@@ -1,13 +1,18 @@
 # 5) Crie um script que busque no google. Utilize a url:
 # http://www.google.com/search. Além disso, passe um parâmetro chamado “q” com o valor a ser buscado.
 
-from selenium import webdriver
+import requests
+from bs4 import BeautifulSoup
 
 busca = input("Conteúdo a ser pesquisado: ")
 
 def buscador(q):
-    navegador = webdriver.Chrome()
-    navegador.get(f"https://www.google.com/search?q={q}")
+    navegador = requests.get(f"https://www.google.com/search?q={q}")
+    soup = BeautifulSoup(navegador.content, "html.parser")
+    h3 = soup.findAll("h3")
+    print("Principais resultados: ")
+    for h in h3:
+        print("-", h.get_text())
 
 buscador(busca)
 
