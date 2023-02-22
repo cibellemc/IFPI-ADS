3b449a1667ca8c9df6a1f54d93d54afbf06af9cd
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-02-22 00:01:22 -0300

    fix: nome da imagem

--- Mod-III/ProgramacaoWeb/exercicio-01/q4.py
+++ Mod-III/ProgramacaoWeb/exercicio-01/q4.py
@@ -6,4 +6,4 @@ import urllib.request as ur
 # https://s2.glbimg.com/ZnzfgTpi8WIHpSj1XGTQd_490oY=/1200x/smart/filters:cover():strip_icc()/i.s3.glbimg.com/v1/AUTH_7d5b9b5029304d27b7ef8a7f28b4d70f/internal_photos/bs/2022/h/k/RACBH1SHaBKfUOt4KjTQ/hans-jurgen-mager-qqwv91ttbre-unsplash.jpg
 url = input("URL da imagem: ")
 
-ur.urlretrieve(url, "exercicio-01/img-q3.jpg")
+ur.urlretrieve(url, "exercicio-01/img-q4.jpg")
