45ae107056cacb1740b3cbe41ffbfe1502569053
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-03-01 20:54:51 -0300

    captura frases com a palavra chave

--- Mod-III/ProgramacaoWeb/exercicio-02/q2.py
+++ Mod-III/ProgramacaoWeb/exercicio-02/q2.py
@@ -4,6 +4,7 @@ import requests_cache
 from bs4 import BeautifulSoup
 
 # https://sites.pitt.edu/~naraehan/python3/re.html
+# https://python-forum.io/thread-16722.html
 
 requests_cache.install_cache('cache')
 
@@ -20,9 +21,20 @@ def main():
 def search(keyword, url, depth):
     page = requests.get(f"{url}", verify=False)
     soup = BeautifulSoup(page.content, 'html.parser').text
-    result = re.findall(keyword, str(soup))
+    result = re.findall(fr'.*{keyword}.*', str(soup))
     print(result) #print de array
 
+
+"""def contains_word(text):
+    return text and the_word in text
+ 
+soup = BeautifulSoup(html, 'lxml')
+the_word = 'python'
+tags_found = soup.find_all(re.compile(".*"), text=contains_word)
+print(tags_found)
+print('-' * 15)
+print([s.text for s in tags_found])
+"""
 main()
 
 
