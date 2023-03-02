b4d8eb4b65310dd00db00a5e8094ad66189631d5
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-03-01 22:22:42 -0300

    verifica profundidade e printa links

--- Mod-III/ProgramacaoWeb/exercicio-02/q2.py
+++ Mod-III/ProgramacaoWeb/exercicio-02/q2.py
@@ -29,16 +29,27 @@ def ask_depth():
     return profundidade
 
 
-def search(keyword, url, depth):
-    page = requests.get(f"{url}", verify=False)
-    soup = BeautifulSoup(page.content, 'html.parser').text
+def search(keyword, url, depth):  
+    page = requests.get(f"{url}", verify=False) # entra na página
+    soup = BeautifulSoup(page.content, 'html.parser') # pega html
 
-    # ignora palavras que não têm os 20 antes/depois
-    # 19 porque adicionei os espaços antes de passar o parâmetro
-    result = re.findall(f'.{{19}}{keyword}.{{19}}', str(soup), re.IGNORECASE)
+    if depth == 0:
+        getchar(keyword, soup.text) # palavra chave no "texto"
+    else:
+        tagsA = soup.findAll('a') # procura todos os links
 
-    for r in result:
-        print(r)
+        for i in range(depth + 1): # pega os que estão na profundidade
+            getchar(keyword, tagsA[i])
+            print("- ", tagsA[i].get("href"))
+            
+
+def getchar(keyword, soup):
+        # ignora palavras que não têm os 20 antes/depois
+        # 19 porque adicionei os espaços antes de passar o parâmetro
+        result = re.findall(f'.{{19}}{keyword}.{{19}}', str(soup), re.IGNORECASE)
+
+        for r in result:
+            print(r)
 
 main()
 
