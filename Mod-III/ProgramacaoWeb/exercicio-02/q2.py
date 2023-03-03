4c81cdf74da32431181f42d9eb23fd5ff904c0e0
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-03-03 06:04:57 -0300

    testes com dicionário

--- Mod-III/ProgramacaoWeb/exercicio-02/q2.py
+++ Mod-III/ProgramacaoWeb/exercicio-02/q2.py
@@ -1,18 +1,19 @@
 import re
-from time import sleep
+import urllib3
 import requests
 import requests_cache
 from bs4 import BeautifulSoup
 requests_cache.install_cache('cache')
-from urllib.request import urlopen
-import urllib3
+from requests.exceptions import MissingSchema
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+from urllib.request import urlopen
 
 # https://sites.pitt.edu/~naraehan/python3/re.html
 # https://python-forum.io/thread-16722.html
 # https://www.handtalk.me/br/blog/leis-de-acessibilidade/ -> site teste
-# se encontra do lado direito da sua tela e conhe
 
+# enquantto a busca for menor que 0, pede novamente profundidade
 def ask_depth():
     profundidade = int(input("Profundidade da busca: "))
 
@@ -23,26 +24,36 @@ def ask_depth():
 
 
 def search(keyword, url, depth):  
-    sites_visitados.append(url)
-    print(f"\nPágina {depth} - {url}")
-    page = requests.get(f"{url}", verify=False) # entra na página
-    soup = BeautifulSoup(page.content, 'html.parser') # pega html
+    sites_visitados.append(url) # cada site visitado (recebido como argumento)
+    dicionario_links[url] = 0
+    links.clear() # importante ser array vazio na chamada recursiva
 
-    dicionario_links
+    print(f"\nPágina {depth} - {url}") 
+    try:
+        page = requests.get(f"{url}", verify=False) 
+        soup = BeautifulSoup(page.content, 'html.parser') 
 
-    getchar(keyword, soup.text) # palavra chave no "texto"
+        novo_link = '' 
 
-    tagsA = soup.find_all('a', attrs={'href': re.compile("^http.*")}) # procura todos os links clicáveis
-    for tag in tagsA:
-        l = tag.get('href')
-        links.append(l)
+        getchar(keyword, soup.text) # palavra chave no "texto"
 
-    print(links)
-    if depth > 1:
-        # pra cada link na página atual
-        for link in links: 
-            if link not in sites_visitados:
-                search(keyword, link, depth - 1)
+        tagsA = soup.find_all('a', attrs={'href': re.compile("^http.*")}) # procura todos os links clicáveis
+        for tag in tagsA:
+            l = tag.get('href') # extrai todos os sites para um array externo
+            links.append(l)
+
+        if depth > 1:
+            # pra cada link na página atual
+            for link in links: 
+                if link in sites_visitados:
+                    dicionario_links[link] += 1 # se faz referência
+                if link not in sites_visitados: # se o link ainda não foi buscado
+                    novo_link = link
+                    break
+            search(keyword, novo_link, depth - 1)
+
+    except MissingSchema:
+        print("Não é possível avançar") # casos de imagem
 
 
 # prioridade positiva: quantidade de ocorrências
@@ -61,8 +72,10 @@ def getchar(keyword, soup):
         for r in result:
             print(r)
 
+
 links = [] # auxiliar p/ permitir recursividade
 sites_visitados = [] # array de links que o buscador entra
+
 
 print("--- Buscador Online ---\n")
 
@@ -75,6 +88,7 @@ profundidade = ask_depth()
 search(keyword, site, profundidade)
 
 print(dicionario_links)
+
 
 
 
