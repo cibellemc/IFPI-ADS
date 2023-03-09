9e41d540909c0bd27853e27f875eed8a8d5c42c5
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-03-09 01:09:28 -0300

    testes de nick

--- Mod-III/ProgramacaoWeb/jogo-em-rede/cliente.ts
+++ Mod-III/ProgramacaoWeb/jogo-em-rede/cliente.ts
@@ -1,26 +1,17 @@
 import * as net from 'net';
-const readline = require('readline')
 
-// aparece no terminal do cliente
 const client: net.Socket = new net.Socket();
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
-})
 
 client.connect(3000, 'localhost', () => {
    console.log('Conectado ao servidor');
-   rl.addListener("line", (line: string | Uint8Array) => {
-       client.write(line) // escreve para servidor
-   })
-   
+   client.write('Olá, eu sou o cliente');
 });
 
-/*client.on('data', (data: Buffer) => {
+client.on('data', (data: Buffer, username:string) => {
    console.log(`Mensagem do servidor: ${data.toString()}`);
-   client.write('Vou desconectar');
-   client.end();
-});*/
+   client.write(data)
+   //client.end();
+});
 
 client.on('end', () => {
    console.log('Desconectado do servidor');
--- Mod-III/ProgramacaoWeb/jogo-em-rede/server.ts
+++ Mod-III/ProgramacaoWeb/jogo-em-rede/server.ts
@@ -1,23 +1,75 @@
 import * as net from 'net';
+const input = require('prompt-sync')()
+// import { v4 as uuidv4 } from 'uuid';
+let count: number = 0
 
-// aparece no terminal do servidor
-const server: net.Server = net.createServer((socket: net.Socket) => {
-   console.log(`Cliente conectado: ${socket.remoteAddress}:${socket.remotePort}`);
-   socket.write('Olá, cliente!\n'); // escreve para cliente
-   
+const server: net.Server = net.createServer()
+
+interface Jogador {
+  id: number;
+  socket: net.Socket;
+  nickname?: string;
+}
+ 
+ interface Mensagem {
+   de: string;
+   conteudo: string;
+ }
+ 
+ const enviarMensagem = (mensagem: Mensagem, remetenteId: number) => {
+   const conteudo = `${mensagem.de}: ${mensagem.conteudo}\n`;
+   jogadores.forEach((jogador) => {
+     if (jogador.id !== remetenteId) {
+       jogador.socket.write(conteudo);
+     }
+   });
+ };
+
+const jogadores: Map<number, Jogador> = new Map();
+
+server.on('connection', socket => {  
+  const playerId = count++;
+  const jogador: Jogador = { id: playerId, socket: socket };
+  jogadores.set(playerId, jogador);
+
+   console.log(`Cliente conectado: ${playerId}:${socket.remoteAddress}`); // escreve para o server
+   jogador.socket.write('Bem-vindo! Por favor, insira um nickname: ');
+   // data event
    socket.on('data', data => {
-        const str = data.toString()
-        if(str === 'end'){
-            socket.end()
+      const message = data.toString().trim();
+      console.log(`Mensagem recebida do jogador ${playerId}: ${message}`);
+
+      // Se o jogador não tiver um nickname, armazene a mensagem como o apelido do jogador
+      /*if (message === 'sair' || message === 'quit') {
+        console.log(`Jogador desconectado: ${playerId}`);
+        socket.end(); // desconecta o cliente do socket
+      }
+      } else {*/
+
+        if (!jogador.nickname) {
+          jogador.nickname = message;
+          console.log(`Jogador ${playerId} é agora conhecido como ${jogador.nickname}`);
+          jogador.socket.write(`Bem-vindo, ${jogador.nickname}!\n`);
+        } else {
+          const conteudo = `${jogador.nickname}: ${message}\n`;
+          // const mensagem: Mensagem = JSON.parse(conteudo);
+          enviarMensagem((conteudo), playerId);
         }
-       console.log(str);
+        // Código para processar a mensagem recebida do jogador
+
+      
+  
     });
     
-    socket.on('end', () => {
-       console.log('Cliente desconectado');
-    });
+   // emite o fim do evento
+   socket.on('end', () => {
+      console.log(`Jogador desconectado: ${playerId}`);
+      jogadores.delete(playerId);
+   });
     
-});
+   });
+
+   
 
 server.listen(3000, () => {
    console.log('Servidor inicializado na porta 3000'); // escutar página - 1ª mensagem
