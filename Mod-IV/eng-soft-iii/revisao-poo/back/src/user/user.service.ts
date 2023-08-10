07d4e1b9a48b0597cc01e4009fcc89fe60a527c5
Cibelle Maciel <cibellemaciel02@gmail.com>
2023-08-10 20:08:57 -0300

    add: back-end com nest

---
+++ Mod-IV/eng-soft-iii/revisao-poo/back/src/user/user.service.ts
@@ -0,0 +1,17 @@
+// src/user/user.service.ts
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './user.model';
+
+@Injectable()
+export class UserService {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(user: User): Promise<User> {
+    return await this.userRepository.save(user);
+  }
+}
